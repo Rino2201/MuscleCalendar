f8aa49a079e03923300e2339d7a18805998cc709
NORIAKI <babababargxx@gmail.com>
2022-03-13 14:31:57 +0000

    [ADD]routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    root to: "homes#top"
+    get "/about", to: "homes#about"
+    resources :users, only:[:index, :show, :edit, :update] do
+      collection do
+        patch "out"
+      end
+    end
+    resources :tweets
+    resources :trainings
+    resources :menus, only:[:index, :create, :edit, :update]
+    resources :parts, only:[:index, :create, :edit, :update]
 end
