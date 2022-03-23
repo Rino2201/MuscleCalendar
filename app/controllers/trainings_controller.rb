f4053119a409b0d9c43014835c2d08897e4da37a
NORIAKI <babababargxx@gmail.com>
2022-03-23 14:58:33 +0000

    [ADD]training_fanction

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -1,13 +1,41 @@
 class TrainingsController < ApplicationController
+
   def new
+    @training = Training.new
+  end
+
+  def create
+    @training = Training.new(training_params)
+    @training.user_id = current_user.id
+    if @training.save
+      redirect_to trainings_path
+    else
+      render :index
+    end
   end
 
   def show
   end
 
   def index
+    @trainings = Training.all
   end
 
   def edit
+    @training = Training.find(params[:id])
   end
+
+  def update
+    @training = Training.find(params[:id])
+    if @training.update(training_params)
+      redirect_to trainings_path
+    else
+      render :edit
+    end
+  end
+
+  def training_params
+    params.require(:training).permit(:user_id, :menu_id, :weight, :rep, :set)
+  end
+
 end
